032bf66fa962b92420871181dc53ef5127d4d446
Andrew Sager <andrew.sager06@gmail.com>
2017-08-10 12:43:14 -0400

    Adding descriptive comments

--- completeparse.py
+++ completeparse.py
@@ -1,3 +1,6 @@
+#Andrew Sager
+#8/10/2017
+#run this file, not the other .py files!\
 import csv
 import singlefarms
 import multiplefarms
@@ -23,8 +26,8 @@ def add_feature(A): #performs json formatting for each individual row
 }""" % (str(A[2]),str(A[0]),str(A[1])) #in order: coordinates, member_id, farm_id
 	return out #return the new string we've made
 
-all_farms = []
-all_farms.extend(singlefarms.get_farms())
+all_farms = [] #grab data from the other .py files
+all_farms.extend(singlefarms.get_farms()) 
 all_farms.extend(multiplefarms.get_farms())
 
 #setting up syntax for the JSON:
@@ -36,9 +39,9 @@ for i in range(len(all_farms)):
 	s += add_feature(all_farms[i])
 	if (i != len(all_farms)-1): s += ", \n\n" #last farmer doesn't need a comma after his details
 
-s += "\n	]\n}"
+s += "\n	]\n}" #cap the JSON
 
 output_file = open("treefarm_coordinates.geojson", "w") #writing the .geojson file
 output_file.write(s)
 output_file.close()
-print("All coordinates were added to the file \'treefarm_coordinates.geojson\'.") #confirmation message
+print("All coordinates were added to the file treefarm_coordinates.geojson\'.") #confirmation message
